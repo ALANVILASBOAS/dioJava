4174a7ddc418056fb575c0a440492396cabb0f25
ALLANVILASBOAS <alan07vb@gmail.com>
2023-03-14 11:42:34 -0300

    second commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,10 +6,48 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
         int a = 5, b = 3;
 
+        Livro livro = new livro(nome;: "Dragões de Éter", numPaginas:300);
+        System.out.println(livro1);
 
         Gato gato = new Gato();
+
         System.out.println("Hello word!" + (a + b));
         System.out.println(gato);
     }
 
+}
+
+
+class livro{
+    private String nome;
+    private Integer numPaginas;
+
+    public livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
 }
